4334174e8d61265f2c365124a175b00b65311dbd
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-03-17 21:50:18 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,9 @@
 # email-slicer
-# This is a code for an email slicer where it slice it into two parts
+# This is a code for an email slicer where it slices the email into two parts; it separates the username from the domain name of an username
 
 email = input("Enter Your Email Address Here: ").strip()
 username = email[:email.index("@")]
-domain_name = email[email.index("@") + 1 :]
+domain_name = email[email.index("@") + 1:]
 outcome_ = (f"Your username is '{username}' and your domain is '{domain_name}'")
 
 print(outcome_)
